1396772b7d0fe03dcea4771194ee317f0a509015
zhangxh614 <zhangxh614@gmail.com>
2017-07-03 22:32:29 +0800

    led

--- Arduino_code/rollingLED/rollingLED.ino
+++ Arduino_code/rollingLED/rollingLED.ino
@@ -1,6 +1,6 @@
 #include <LedControl.h>  
 #include <SoftwareSerial.h>
-
+//ble TX->D12 RX->D9 +5V->5V GND->GND
 int DIN = 11;  
 int CS =  10;  
 int CLK = 13;  
@@ -26,23 +26,48 @@ LedControl lc=LedControl(DIN,CLK,CS,4);
   
 void setup(){  
  Serial.begin(9600);
-     
  delay(100);
+ pinMode(A3,OUTPUT);//R
+ pinMode(A5,OUTPUT);//G
+ pinMode(A6,OUTPUT);//B
+ digitalWrite(A3,LOW);
+ digitalWrite(A5,LOW);
+ digitalWrite(A6,LOW);
  lc.shutdown(0,false);       //启动时，MAX72XX处于省电模式  
  lc.setIntensity(0,2);       //将亮度设置为合适的值(0-8）
  lc.clearDisplay(0);         //清除显示  
 }  
   
 void loop(){
+  
   int k=0;
   while(Serial.available()>0) {
     msg[k++]=Serial.read();
   }
-  Serial.println((int)msg[0]-48);
-  Serial.println((int)msg[1]-48);
-  Serial.println((int)msg[2]-48);
-  Serial.println((int)msg[3]-48);
+  //Serial.println((int)msg[0]-48);
+  //Serial.println((int)msg[1]-48);
+  //Serial.println((int)msg[2]-48);
+  //Serial.println((int)msg[3]-48);
   add((int)msg[0]-48,(int)msg[1]-48,(int)msg[2]-48,(int)msg[3]-48);
+  if(msg[4]=='1') {
+    digitalWrite(A3,HIGH);
+    digitalWrite(A5,LOW);
+    digitalWrite(A6,LOW);
+    Serial.println("red");
+  }
+  else if(msg[4]=='2') {
+    digitalWrite(A3,LOW);
+    digitalWrite(A5,HIGH);
+    digitalWrite(A6,LOW);
+    Serial.println("gr");
+  }
+  else { 
+    digitalWrite(A3,LOW);
+    digitalWrite(A5,LOW);
+    digitalWrite(A6,LOW);
+    Serial.println("an");
+  }
+  
   display(ans,19);
 }  
   
